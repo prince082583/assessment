53566f8930388aeebe636e4374d5c1cad2a5200f
Prince Jose <prince.jose@ibm.com>
2021-04-05 17:47:39 +0530

    assessment

--- exercise-1/app.js
+++ exercise-1/app.js
@@ -22,15 +22,3 @@ function showDetails(id){
 $("#userdetail-"+id).show();
 $("#show-detail-"+id).hide();
 }
-function deleteUser(id){
-if (!confirm("Do you want to delete")){
-return false;
-}
-$.ajax({
-url: 'https://5dc588200bbd050014fb8ae1.mockapi.io/assessment/'+id,
-type: 'DELETE',
-success: function(result) {
-location.reload();
-}
-});
-}
